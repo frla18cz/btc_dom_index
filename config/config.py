4b32156aea5c1483d5f666d56f4166e48a8db135
frla18cz <70630255+frla18cz@users.noreply.github.com>
2025-05-04 11:34:44 +0200

    Refactor: Move config to config/ and fetcher to fetcher.py

---
+++ config/config.py
@@ -0,0 +1,16 @@
+import datetime as dt
+
+# --- Data Fetching Configuration ---
+LIMIT = 100  # Max allowed by CryptoCompare API for this endpoint
+TSYM = "BTC"  # Target symbol for market cap calculation
+REQUEST_TIMEOUT = 15 # seconds
+API_SLEEP_INTERVAL = 1 # seconds between API calls
+TOP_N = 20 # Number of top coins to select
+EXCLUDED_SYMBOLS = ['BTC', 'USDT', 'USDC', 'DAI', 'BUSD','USDE', 'USDS', 'WBETH', 'WEETH'] # Symbols to exclude from the top 20
+
+# --- Output Configuration ---
+OUTPUT_FILENAME = "btcdom_top20_weights.csv"
+
+# --- Time Range Configuration ---
+START = dt.datetime(2025, 2, 17, 8)  # Start date for fetching data
+END = dt.datetime(2025, 4, 24, 8)  # End date for fetching data
