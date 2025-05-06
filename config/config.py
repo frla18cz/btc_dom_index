ff9c0d1459028ce6e6d6c9d19f5b58849b1a907e
frla18cz <70630255+frla18cz@users.noreply.github.com>
2025-05-06 10:26:06 +0300

    update - odstranovani

--- config/config.py
+++ config/config.py
@@ -5,11 +5,11 @@ LIMIT = 100  # Max allowed by CryptoCompare API for this endpoint
 TSYM = "BTC"  # Target symbol for market cap calculation
 REQUEST_TIMEOUT = 15 # seconds
 API_SLEEP_INTERVAL = 1 # seconds between API calls
-TOP_N = 20 # Number of top coins to select
+TOP_N = 50 # Number of top coins to select
 EXCLUDED_SYMBOLS = ['BTC', 'USDT', 'USDC', 'DAI', 'BUSD','USDE', 'USDS', 'WBETH', 'WEETH'] # Symbols to exclude from the top 20
 
 # --- Output Configuration ---
-OUTPUT_FILENAME = "btcdom_top20_weights.csv"
+OUTPUT_FILENAME = "btcdom_top50_weights.csv"
 DB_PATH = "btcdom_data.db"
 
 # --- Time Range Configuration ---
